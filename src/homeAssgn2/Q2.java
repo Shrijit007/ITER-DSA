44eb88f59562c9652a595fccfb45db6e31ebbf11
Shrijit007 <shrijitraj2023@gmail.com>
2024-04-17 19:10:55 +0530

    updated

--- src/homeAssgn2/Q2.java
+++ src/homeAssgn2/Q2.java
@@ -15,6 +15,7 @@ class Book{
         System.out.println("Name: "+BName+" Edition: "+BEdition+" Price: "+BPrice);
     }
 }
+
 public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
