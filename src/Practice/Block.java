25dcca5f9300e68d3bd2b7281266bc4efd420d63
Shrijit007 <shrijitraj2023@gmail.com>
2024-04-18 11:55:17 +0530

    updated

---
+++ src/Practice/Block.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Block {
+    static {
+        System.out.println("Ouu Maa Guu Turu LOB🧡");
+    }
+    {
+        System.out.println("Ye saare milkr hume pagal bna rahe h😠");
+    }
+    public static void main(String[] args) {
+        Block b1 = new Block();
+        try{ } finally {
+            System.out.println("Pura thela laal🍎 haiii");
+        }
+        System.out.println("Mujhe drugs doooo💊");
+        Block b2 = new Block();
+
+    }
+}
