62a6707f8f4cd940beb882b714f6fc2b52586ae8
Shrijit007 <shrijitraj2023@gmail.com>
2024-04-18 12:29:13 +0530

    updated

---
+++ src/Practice/ConstructorOverloading.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class ConstructorOverloading {
+    int x;
+    int y;
+    ConstructorOverloading(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    ConstructorOverloading(int x,int y,int q){
+        this.x=x;
+        this.y=y;
+    }
+}
