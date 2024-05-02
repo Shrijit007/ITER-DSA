f0d6731cacce62bc80a5595432c6157224e658d7
Shrijit007 <shrijitraj2023@gmail.com>
2024-05-02 17:23:58 +0530

    updated

---
+++ src/Practice/BinarySearch.java
@@ -0,0 +1,29 @@
+package Practice;
+class BinarySearch {
+    int binarySearch(int arr[], int x)      // Returns index of x if it is present in arr[].
+    {
+        int l = 0, r = arr.length;
+        while (l <= r) {
+            int m = (r + l) / 2;
+            if (arr[m] == x)
+                return m;
+            if (arr[m] < x)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = { 2, 3, 4, 10, 40 };
+        int n = arr.length;
+        int x = 10;
+        int result = ob.binarySearch(arr, x);
+        if (result == -1)
+            System.out.println("Element is not present in array");
+        else
+            System.out.println("Element is present at "+ "index " + result);
+    }
+}
