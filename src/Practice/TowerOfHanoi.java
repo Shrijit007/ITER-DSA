7b37b341cf7b670644e7b31230564422cbb8241a
Shrijit007 <shrijitraj2023@gmail.com>
2024-05-02 10:35:27 +0530

    updated

---
+++ src/Practice/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class TowerOfHanoi{
+    static void TowerOfHanoi(int n,char from,char to,char aux){
+        if(n==1){
+            System.out.println("move disk 1 from "+from+" to "+to);
+            return;
+        }
+        TowerOfHanoi(n-1,from,aux,to);
+        System.out.println("move disk "+n+" from "+from+" to "+to);
+        TowerOfHanoi(n-1,aux,to,from);
+    }
+
+    public static void main(String[] args) {
+        int n=4;
+        TowerOfHanoi(n,'A','B','C');
+    }
+}
