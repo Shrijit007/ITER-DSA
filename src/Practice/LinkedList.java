5ca8afcb66e5f97e061e4e964bd5c445eeec3934
Shrijit007 <shrijitraj2023@gmail.com>
2024-05-04 00:50:27 +0530

    updated

--- src/Practice/LinkedList.java
+++ src/Practice/LinkedList.java
@@ -1,61 +1,48 @@
 package Practice;
 import java.util.*;
-class node
-{
-int info;
-node next;
+class node {
+    int info;
+    node next;
 }
-public class LinkedList
-{
+public class LinkedList {
     static node start=null;
-    public static void create()
-    {
- Scanner sc=new Scanner(System.in);
- node p=new node();
- System.out.println("Enter info");
- p.info=sc.nextInt();
- p.next=null;
- start=p;
- node q=p;
- System.out.println("Do you want to create more number of nodes(y/n)");
- char ch=sc.next().charAt(0);
- while(ch!='n')
- {
- p=new node();
- System.out.println("Enter info");
- p.info=sc.nextInt();
- p.next=null;
- q.next=p;
- q=p;
- System.out.println("Do you want to create more number of nodes(y/n)");
- ch=sc.next().charAt(0);
-}
+    public static void create() {
+        Scanner sc=new Scanner(System.in);
+        node p=new node();
+        System.out.println("Enter info");
+        p.info=sc.nextInt();
+        p.next=null;
+        start=p;
+        node q=p;
+        System.out.println("Do you want to create more number of nodes(y/n)");
+        char ch=sc.next().charAt(0);
+        while(ch!='n') {
+            p=new node();
+            System.out.println("Enter info");
+            p.info=sc.nextInt();
+            p.next=null;
+            q.next=p;
+            q=p;
+            System.out.println("Do you want to create more number of nodes(y/n)");
+            ch=sc.next().charAt(0);
+        }
     }
-    public static void display()
-    {
- node p=start;
- while(p!=null)
- {
- System.out.print(p.info+"-->");
- p=p.next;
- }
-
+    public static void display() {
+        node p=start;
+        while(p!=null) {
+            System.out.print(p.info+"-->");
+            p=p.next;
+        }
     }
-
-    /* Code for the remaining user defined methods*/
-
-    public static void main(String[] args)
-    {
-
- while(true)
- {
- System.out.println("\n*MENU");
- System.out.println("0:Exit");
- System.out.println("1:Creation");
- System.out.println("2:Display");
- Scanner sc=new Scanner(System.in);
- System.out.println("Enter the choice");
- int choice=sc.nextInt();
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("\n*MENU");
+            System.out.println("0:Exit");
+            System.out.println("1:Creation");
+            System.out.println("2:Display");
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the choice");
+            int choice=sc.nextInt();
  switch(choice)
  {
 case 0:
