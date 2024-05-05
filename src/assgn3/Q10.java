dd0dc688ae2cc57129fd8f76cc10069304054ccd
Shrijit007 <shrijitraj2023@gmail.com>
2024-05-05 10:37:56 +0530

    updated

---
+++ src/assgn3/Q10.java
@@ -0,0 +1,13 @@
+package assgn3;
+
+public class Q10 {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+    public static int fibonacci(int n){
+        if(n <= 1)
+            return n;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
