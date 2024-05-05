dfa9ba7357ba628876b4d40cc4d5e3c4c81776b3
Shrijit007 <shrijitraj2023@gmail.com>
2024-05-05 10:37:36 +0530

    updated

---
+++ src/assgn3/Q9.java
@@ -0,0 +1,15 @@
+package assgn3;
+
+public class Q9 {
+    public static void main(String[] args) {
+        reverse(1257);
+    }
+    public static void reverse(int n){
+        if(n < 10){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n%10);
+        reverse(n/10);
+    }
+}
