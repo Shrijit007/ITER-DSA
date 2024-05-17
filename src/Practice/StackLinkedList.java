be059d6d10f1a925c2a8fa04f0823ca0d263ffef
Shrijit007 <shrijitraj2023@gmail.com>
2024-05-18 00:45:06 +0530

    updated

---
+++ src/Practice/StackLinkedList.java
@@ -0,0 +1,73 @@
+package Practice;
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class StackLinkedList{
+    private Node top;
+
+    public StackLinkedList() {
+        this.top = null;
+    }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        newNode.next = top;
+        top = newNode;
+        System.out.println("Pushed " + value + " into the stack");
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return;
+        }
+        System.out.print("Stack: ");
+        Node current = top;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        StackLinkedList stack = new StackLinkedList();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.display();
+        System.out.println("Popped: " + stack.pop());
+        stack.display();
+        System.out.println("Top element: " + stack.peek());
+    }
+}
+
