013d992fa0c3e8d1a5988f4175f3c86f1d808147
Shrijit007 <shrijitraj2023@gmail.com>
2024-06-08 17:14:53 +0530

    updated

---
+++ src/homeAssgn3/Q6.java
@@ -0,0 +1,22 @@
+//Write a recursive Java method that determines if a string s is a palindrome, that is, it is
+//equal to its reverse.
+
+package homeAssgn3;
+public class Q6 {
+    public static boolean isPalindrome(String s) {
+        if (s.length() <= 1)
+            return true;
+        if (s.charAt(0) == s.charAt(s.length() - 1))
+            return isPalindrome(s.substring(1, s.length() - 1));
+        return false;
+    }
+    public static void main(String[] args) {
+        String palindrome1 = "racecar";
+        String palindrome2 = "gohangasalamiimalasagnahog";
+        String nonPalindrome = "hello";
+
+        System.out.println(palindrome1 + " is a palindrome: " + isPalindrome(palindrome1));
+        System.out.println(palindrome2 + " is a palindrome: " + isPalindrome(palindrome2));
+        System.out.println(nonPalindrome + " is a palindrome: " + isPalindrome(nonPalindrome));
+    }
+}
