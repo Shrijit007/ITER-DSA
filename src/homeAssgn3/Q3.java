9fd9d422745fa6058953621f331ca89aa3d43805
Shrijit007 <shrijitraj2023@gmail.com>
2024-06-08 17:05:42 +0530

    updated

---
+++ src/homeAssgn3/Q3.java
@@ -0,0 +1,22 @@
+package homeAssgn3;
+
+public class Q3 {
+
+    // Recursive method to find binary equivalent of a positive decimal integer
+    public static String decimalToBinary(int n) {
+        if (n == 0) {
+            return "0";
+        }
+        if (n == 1) {
+            return "1";
+        }
+        return decimalToBinary(n / 2) + (n % 2);
+    }
+
+    public static void main(String[] args) {
+        int number = 13;
+        String binaryRepresentation = decimalToBinary(number);
+        System.out.println("Binary equivalent of " + number + " is: " + binaryRepresentation);
+    }
+}
+
